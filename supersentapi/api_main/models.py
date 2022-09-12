8e0a8004bc895847be0019642e785507d60201ed
nickesparza <nickesparza@gmail.com>
2022-09-12 14:47:35 -0500

    add Team model

--- supersentapi/api_main/models.py
+++ supersentapi/api_main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# model for individual sentai teams
+class Team(models.Model):
+    series_number = models.IntegerField()
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    era = models.CharField(max_length=25)
+    colors = models.JSONField(default=list)
+
+    def __str__(self):
+        return self.name
