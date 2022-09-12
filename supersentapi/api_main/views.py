e555d3edb3f26faa5024f20a335e227217683ab2
nickesparza <nickesparza@gmail.com>
2022-09-12 15:08:56 -0500

    add /teams endpoint for list of all sentai teams and get JSON back

--- supersentapi/api_main/views.py
+++ supersentapi/api_main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Team
 
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Super SentAPI</h1>')
+
+def get_teams(request):
+    teams = Team.objects.all().values()
+    return JsonResponse({"teams": list(teams)})
